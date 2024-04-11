ef144a41dc10ffcc9206cc802b08d19a82840a0a
Rich-VH <ricardo.valdez@wizeline.com>
2024-04-11 16:45:52 -0600

    Initializing the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Demo repo for the MLOps Bootcamp',
+    author='Rich V',
+    license='',
+)
